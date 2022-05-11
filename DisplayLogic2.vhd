e66409ce658fa374b852d86dee762aed0a7aecfb
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2022-05-11 14:40:47 +0200

    Place big images

--- DisplayLogic2.vhd
+++ DisplayLogic2.vhd
@@ -29,10 +29,10 @@ constant BLACK : RGB_type := ToRGB(X"000000");  -- or ToRGB(0,0,0);
 ---------------------------------------------------------------------------
 constant MEMROWSIZE : integer := 256; -- memory organization
 constant MEMROWCOUNT : integer := 256;
-constant EMBORGX1 : integer := 64; -- positions of picture in the flag
-constant EMBORGY1 : integer := 64;
-constant EMBORGX2 : integer := XSCREEN-MEMROWSIZE-32; -- positions of picture in the flag
-constant EMBORGY2 : integer := YSCREEN-MEMROWCOUNT-32;
+constant EMBORGX1 : integer := 0; -- positions of picture in the flag
+constant EMBORGY1 : integer := YSCREEN - MEMROWCOUNT;
+constant EMBORGX2 : integer := XSCREEN - MEMROWSIZE; -- positions of picture in the flag
+constant EMBORGY2 : integer := 0;
 constant MEM_END_ADDRESS : integer := 65535;
 
 component romPicture256x256 IS
@@ -59,32 +59,23 @@ begin -- architecture
     LSPflag : process(xcolumn, yrow, picture_q_s) 
     variable RGB : RGB_type; -- output colors
     variable x, y : integer; -- renamed xcolumn and yrow
-	 variable romID:integer range 0 to 2;
+	 variable romID : integer;
+	 
     begin
 		x:=to_integer(xcolumn); y:=to_integer(yrow); -- convert to integers
-      
-		--romID:=0;
-		--if(x>=EMBORGX1 and x<EMBORGX1+MEMROWSIZE 
-		  -- and y>=EMBORGY1 and y<EMBORGY1+MEMROWCOUNT) then romID:=1;
-		--end if;
 		
-		--if(x>=EMBORGX2 and x<EMBORGX2+MEMROWSIZE 
-		  -- and y>=EMBORGY2 and y<EMBORGY2+MEMROWCOUNT) then romID:=2;
-		--end if;
+		romID:=0;
+		if(x>=EMBORGX1 and x<EMBORGX1+MEMROWSIZE 
+		   and y>=EMBORGY1 and y<EMBORGY1+MEMROWCOUNT) then romID:=1;
+		end if;
 		
-		--if picture_q_s /= "00000000" then
-		  -- RGB:=BACKGROUND;
-		--end if;
-					
+		if(x>=EMBORGX2 and x<EMBORGX2+MEMROWSIZE 
+		   and y>=EMBORGY2 and y<EMBORGY2+MEMROWCOUNT) then romID:=2;
+		end if;
+		
 		if(x<0) or (x>=XSCREEN) or (y<0) or (y>=YSCREEN) then
 		   RGB:=BLACK; --black outside of visible frame
-		
-		--elsif romID>0 and picture_q_s /= "000000000" then -- no picture background
-		  -- if romID=1 then
-			 -- if picture_q_s = "00001010" then RGB:= BLUE_DARK;
-			 -- elsif picture_q_s = "10011011" then RGB:= BLUE_LIGHT;
-			  --end if;
-			--end if;
+			
 		
 		elsif (x-(XSCREEN/2))*(x-(XSCREEN/2)) + (y-(YSCREEN/2))*(y-(YSCREEN/2)) <= 10000 then
 			if (6*y + 8*x < 250 + 3*YSCREEN + 4*XSCREEN AND 6*y + 250 + 8*x > 3*YSCREEN + 4*XSCREEN) then RGB:=BLUE_DARK;
@@ -92,6 +83,13 @@ begin -- architecture
 			end if;
 		elsif (4*y < 3*x + 250 AND 4*y + 250 > 3*x)
 			then RGB:=VIOLET;
+			
+		elsif (romID /= 0) then
+			if picture_q_s = "00001010" then RGB:= BLUE_DARK;
+				elsif picture_q_s = "10011011" then RGB:= BLUE_LIGHT;
+				else RGB:=BACKGROUND;
+			end if;
+		
 		else 
 		   RGB:=BACKGROUND; -- else is necessary, the complete definition prevents forbidden latches
 		end if;
@@ -102,7 +100,7 @@ begin -- architecture
 		                                                         picture_address_s'LENGTH));
          when 2 =>
 			   -- we rotate rom coordinates by 90 degrees clockwise by matrix [0 1; -1 0]*[x y], i.e, xrom=-y, yrom=x
-			   picture_address_s <= std_logic_vector(to_unsigned((EMBORGX2+MEMROWCOUNT-1-x)*MEMROWSIZE + (y-EMBORGY2),
+			   picture_address_s <= std_logic_vector(to_unsigned((y-EMBORGY2)*MEMROWSIZE + (EMBORGX2-x),
 		                                                         picture_address_s'LENGTH));
 			when others => 
 		      picture_address_s <=(others=>'0'); 
