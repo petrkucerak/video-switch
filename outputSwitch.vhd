5b9ce39c17f2c690fa1afc92fedca6e47e66fcaa
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2022-05-14 12:06:28 +0200

    connected multiplexor with FSM

--- outputSwitch.vhd
+++ outputSwitch.vhd
@@ -10,8 +10,8 @@ use work.VGApackage.all;
 
 entity OutputSwitch is 
 port(	 
-	    CH1, CH2 : in std_logic_vector(23 downto 0);
-		 sw : in std_logic;
+sw : in std_logic_vector(2 downto 0);
+	    CH1, CH2, CH3: in std_logic_vector(23 downto 0);
 		 VGA_R, VGA_G, VGA_B: out vga_byte --  color information
 	 ); 
 end;
@@ -21,9 +21,17 @@ architecture behavioral of OutputSwitch is
 	signal Q : std_logic_vector(23 downto 0);
 
 begin -- architecture
-
-	Q <= CH1 when sw='0' else CH2;
 	
+	Q <= 
+		CH2 when sw="000" else
+		CH2 when sw="001" else
+		CH3 when sw="010" else
+		CH3 when sw="011" else
+		CH2 when sw="100" else
+		CH2 when sw="101" else
+		CH1 when sw="110"
+		else CH1;
+
 	VGA_R <= Q(23 downto 16);
 	VGA_G <= Q(15 downto 8);
 	VGA_B <= Q(7 downto 0);
