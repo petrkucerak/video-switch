78df49f937bd3d139700d6f041f36ffdb928019d
petrkucerak <45851215+petrkucerak@users.noreply.github.com>
2022-05-15 23:28:06 +0200

    Finish all transmissions

--- VideoMixer.vhd
+++ VideoMixer.vhd
@@ -12,9 +12,11 @@ use work.VGApackage.all;
 
 entity VideoMixer is
 
+	generic(T:integer:=250);
+
 	port(
 		state : in std_logic_vector(2 downto 0);
-		xcolumm, yrow : in vga_xy;
+		xcolumn, yrow : in vga_xy;
 		VS_N, HS_N : in std_logic;
 		nextState : out std_logic;
 		Sel : out std_logic_vector (1 downto 0)
@@ -26,6 +28,13 @@ architecture rtl of VideoMixer is
 
 	signal SelE1 : std_logic_vector(1 downto 0);
 	signal SelE2 : std_logic_vector(1 downto 0);
+	signal SelE1R : std_logic_vector(1 downto 0);
+	signal SelE2R : std_logic_vector(1 downto 0);
+	signal nextState0 : std_logic;
+	signal nextState1 : std_logic;
+	signal nextState2 : std_logic;
+	signal nextState3 : std_logic;
+	signal nextState4 : std_logic;
 
 
 begin
@@ -37,13 +46,29 @@ begin
 			-- "101"; -- čekání po dobu T; -> "01"
 			-- "110"; -- proměna z nálepky 2. na 1. efektem opačně běžícím E1;
 			-- "111"; -- čekání po dobu T; -> "00"
-	e1:process(VS_N, state, xcolumm, yrow)
+	tSet:process(VS_N)
+		variable countr : integer range 0 to T;
+		begin
+			if state /= "001" AND state /= "011" AND state /= "101" AND state /= "111" then
+				countr := 0;
+			elsif rising_edge(VS_N) then
+				countr := countr + 1;
+			end if;
+			
+			if countr = T then
+				nextState0<='1';
+			else nextState0<='0';
+			end if;
+			
+	end process;
+	
+	e1:process(VS_N, state, xcolumn, yrow)
 		variable countr : integer range 0 to (XSCREEN)*(XSCREEN);
 		variable x, y : integer; -- renamed xcolumn and yrow
 		variable INC : integer range 0 to XSCREEN * XSCREEN;
 		begin
 			
-			x:=to_integer(xcolumm); y:=to_integer(yrow); -- convert to integers
+			x:=to_integer(xcolumn); y:=to_integer(yrow); -- convert to integers
 			
 			if state /= "000" then
 				countr := 0;
@@ -60,8 +85,90 @@ begin
 			end if;
 			
 			if (XSCREEN * XSCREEN) + (YSCREEN * YSCREEN) <= 4 * countr then -- pyhagoras
-				nextState <= '1';
-			else nextState <= '0';
+				nextState1 <= '1';
+			else nextState1 <= '0';
+			end if;
+	end process;
+	
+	e2:process(VS_N, state, xcolumn, yrow)
+		variable countr : integer range 0 to 4000;
+		variable x, y : integer;
+		variable INC : integer range 0 to XSCREEN;
+		
+		begin
+			x:=to_integer(xcolumn); y:=to_integer(yrow); -- convert to integers
+			
+			if state /= "010" then
+				countr := 0;
+				INC := 25;
+			elsif rising_edge(VS_N) then
+				countr := countr + INC;
+			end if;
+			
+			if (6*y + 8*x < countr + 3*YSCREEN + 4*XSCREEN AND 6*y + countr + 8*x > 3*YSCREEN + 4*XSCREEN) then
+				SelE2 <= "10";
+			else SelE2 <= "01";
+			end if;
+			
+			if  4000 <= countr then
+				nextState2 <= '1';
+			else nextState2 <= '0';
+			end if;
+	
+	end process;
+	
+	e2Reverse:process(VS_N, state, xcolumn, yrow)
+		variable countr : integer range 0 to 4000;
+		variable x, y : integer;
+		variable INC : integer range 0 to XSCREEN;
+		
+		begin
+			x:=to_integer(xcolumn); y:=to_integer(yrow); -- convert to integers
+			
+			if state /= "100" then
+				countr := 4000;
+				INC := 25;
+			elsif rising_edge(VS_N) then
+				countr := countr - INC;
+			end if;
+			
+			if (6*y + 8*x < countr + 3*YSCREEN + 4*XSCREEN AND 6*y + countr + 8*x > 3*YSCREEN + 4*XSCREEN) then
+				SelE2R <= "10";
+			else SelE2R <= "01";
+			end if;
+			
+			if  0 >= countr then
+				nextState3 <= '1';
+			else nextState3 <= '0';
+			end if;
+	
+	end process;
+	
+	e1Reverse:process(VS_N, state, xcolumn, yrow)
+		variable countr : integer range -300 to 160081;
+		variable x, y : integer; -- renamed xcolumn and yrow
+		variable INC : integer range 0 to 2785;
+		begin
+			
+			x:=to_integer(xcolumn); y:=to_integer(yrow); -- convert to integers
+			
+			if state /= "110" then
+				countr := 160080;
+				INC := 2784;
+			elsif rising_edge(VS_N) then
+				countr := countr - INC;
+				INC := INC - 24;
+				
+			end if;
+			
+			if (x-(XSCREEN/2))*(x-(XSCREEN/2)) + (y-(YSCREEN/2))*(y-(YSCREEN/2)) <= countr then
+				SelE1R <= "01";
+			else SelE1R <= "00";
+			end if;
+			
+			if 0 >= countr then -- pyhagoras
+				nextState4 <= '1';
+			else nextState4 <= '0';
 			end if;
 	end process;
 		
@@ -72,11 +179,13 @@ begin
 		-- "001"
 		SelE2 when state="010" else -- normal
 		"11" when state="011" else
-		SelE2 when state="100" else -- reverse
+		SelE2R when state="100" else -- reverse
 		-- "101"
-		SelE1 when state="110" else -- reverse
+		SelE1R when state="110" else -- reverse
 		"00" when state="111"
 		else "01";
+		
+	nextState <= nextState0 OR nextState1 OR nextState2 OR nextState3 OR nextState4;
 		
 
 end rtl;
